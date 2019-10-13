8320276c749215b8c62d894a3d0a24cddf5056af
Cornel Sarbu <sarbu.cornel.traian@gmail.com>
2019-10-13 10:48:20 -0600

    Add files via upload
    - The game has 2 players, playing in rounds
    - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
    - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
    - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score.
         After that, it's the next player's turn
    - The first player to reach 100 points on GLOBAL score wins the game

---
+++ app.js
@@ -0,0 +1,114 @@
+/*
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score.
+     After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+
+var scores, roundScore, dice, gamePlaying ;
+
+init();
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+
+    if(gamePlaying){
+
+        // we need a random number
+    dice = Math.floor(Math.random() * 6) + 1;
+        
+    //display the result
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "dice-"+ dice +".png";
+    
+        
+    //update the round score  if the rolled number was not a 1
+    if(dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    }else{
+        //next player
+        nextPlayer();
+    }  
+
+        }
+
+
+ 
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function(){
+    if(gamePlaying){
+        // add  current score to global score
+    scores[activePlayer] += roundScore;
+    //update the UI
+
+    document.querySelector("#score-" + activePlayer).textContent=scores[activePlayer];
+
+    //check if player wn the game
+
+    if(scores[activePlayer] >= 100){
+        document.querySelector("#name-" + activePlayer).textContent = "WINNER!!";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-"+activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-"+activePlayer + "-panel").classList.remove("active");
+        gamePlaying = false;
+    }else{
+                //next player
+                nextPlayer();
+        }
+
+    }
+    
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+};
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init(){
+
+    gamePlaying=true;
+
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector(".dice").style.display="none";
+    document.getElementById("score-0").textContent ="0";
+    document.getElementById("score-1").textContent ="0";
+    document.getElementById("current-0").textContent ="0";
+    document.getElementById("current-1").textContent ="0";
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
+
+}
+ 
+
+
+
+
+
+
+
+
+
+
